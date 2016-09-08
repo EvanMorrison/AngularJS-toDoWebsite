5642e27d6d645e5c36d57aa9724ff61b1c94cd59
Evan Morrison <evanrmorrison@gmail.com>
2016-09-08 14:57:09 -0600

    add, remove and edit todo items and save changes to the server

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # AngularJS-toDo-Website
+
+V School 7 September, 2016 Exercise, Angular, Web Applications, HTTP/API, Level 2
+
+An exercise in creating a Todo task list website using
+AngularJS. The exercise includes the following basic parameters:
+
+	*	Each todo has a checkbox to marked it complete or incomplete
+	*	Todo items maked complete have a strikethrough
+	*	User can delete a Todo item
+	*	User can edit/update the todo item
+	*	User can attach an imgUrl to the item and the image is displayed
+	*	User can give the item a price
+	*	User can give the item a description
+	*	Website should be nicely laid out with Bootstrap
+
--- app.js
+++ app.js
@@ -1,5 +1,92 @@
 var app = angular.module("ToDoApp", []);
 
-app.controller("ToDoController", function($scope, $http) {
+app.controller("ToDoController",["$scope", "$http", function($scope, $http) {
 	$scope.title = "Angular ToDo Website"
-});
+	$scope.toDoList = [];
+
+	$scope.newTodo = 	{
+							"title" : "",
+							"description"	: "",
+							"price"	: 	"",
+							"imgUrl" : "",
+							"completed" : false
+						}
+
+	$scope.today = new Date();
+
+	$scope.newTodoPopDown = false;
+
+	//validates the user input as numbers - needs to be added
+	$scope.validDollars = function (){
+
+	};
+
+	var baseUrl = 'http://mean.codingcamp.us/evanmorrison/todo/';
+	
+	$scope.getTodos = function() {
+		$http.get(baseUrl)
+		.then(function(response) {
+			$scope.toDoList = response.data;
+			$scope.toDoList.forEach(function(todo) {
+				todo.isBeingEdited = false;
+
+			});
+		console.log($scope.toDoList);
+		});
+	}
+
+	$scope.editTodo = function(index) {
+		console.log(index);
+		$scope.toDoList[index].isBeingEdited = true;
+		console.log($scope.toDoList[index]);
+
+	}
+
+	$scope.postTodo = function(newTodo) {
+		$http.post(baseUrl, newTodo)
+		.then(function(response) {
+			$scope.toDoList.push(response.data);
+			newTodo.isBeingEdited = false;
+			$scope.newTodo = {};
+		})
+	}
+
+	$scope.putTodo = function(todo, index) {
+		console.log('from putTodo function ');
+		console.log(todo);
+		console.log('index : ' + index);
+		$http.put(baseUrl + todo._id, todo)
+		.then(function(response, index) {
+			$scope.toDoList[index] = todo;
+			todo.isBeingEdited = false;
+		});
+	}
+
+
+
+	$scope.deleteTodo = function(todo, index) {
+		var taskId = todo._id;
+		console.log(todo._id);
+		console.log('index: ' + index);
+		$http.delete(baseUrl + taskId)
+		.then(function(response) {
+			$scope.toDoList.forEach(function(item, index) {
+				if (item._id === taskId) {
+					$scope.toDoList.splice(index,1);
+				}
+			});
+
+		})
+	}
+
+	$scope.getTodos();
+}]);
+
+
+
+
+
+
+
+
+
